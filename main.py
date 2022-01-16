78faa91157b776372c1dcf7109fba7679888fa86
Dennis Alp <dalp@kth.se>
2022-01-16 23:48:00 +0100

    add essential image io code

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import sys
 import os
 from pdb import set_trace as st
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+from PIL import Image
+
 def main():
-    st()
+    with Image.open('dat/DSC02536.jpg') as img:
+        exif = img.info['exif']
+        icc = img.info['icc_profile']
+        dpi = img.info['dpi']
+        st()
+        Image.fromarray(np.array(img)).save('dat/test.jpg', exif=exif, icc_profile=icc, dpi=dpi, subsampling=0, quality=96, optimize=True)
 
 if __name__ == '__main__':
     main()
